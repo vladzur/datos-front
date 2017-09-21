6dbe7e84fcb2a925ef004da13c3dc7a011319d37
vladzur <vladzur@gmail.com>
2017-09-21 19:44:13 -0300

    Main structure

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <main-title>Bienvenido a Datos Villarrica</main-title>
+  </div>
+</template>
+
+<script>
+import MainTitle from '../components/MainTitle'
+export default {
+  name: 'home',
+  components: { MainTitle }
+}
+</script>
